afb13a5137856d0b5577b4b3e5afb313b8f3b80f
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-28 13:41:01 -0300

    Simple people register system created

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,12 +2,28 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Datanase
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb', { useMongoClient: true })
 
-// Teste
-server.get('/', (req, res) => res.send('Backend'))
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true },
+})
+
+// REST API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({ new: true, runValidators: true })
+
+// Routes
+Client.register(server, '/clients')
 
 server.listen(3000)
